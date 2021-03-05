7309c3e4aca534d3b71549b25b074db51f3c0d6d
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2021-03-05 16:51:55 -0500

    Fully Functional - Jasmine Incomplete

--- scripts/md5Encrypt.js
+++ scripts/md5Encrypt.js
@@ -1,8 +1,8 @@
 /**
- * MD5 Encryption function to encrypt provided string.
- * Returns string of encrypted hash.
- * @param {string} stringIn
- */
+   * MD5 Encryption function to encrypt provided string.
+   * Returns string of encrypted hash.
+   * @param {string} stringIn
+   */
 function md5Encrypt(stringIn) {
     "use strict";
     var md5string = new CryptoJS.MD5(stringIn);
--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,81 @@
-//On load, fire this function
+/**
+ * ASSIGNMENT 02 - PASSWORD ENCRYPTION
+ * Student: Bryan Hughes
+ * Valid user: SeanDoyle
+ * Valid password: FoiledByDoyle3030
+ */
+
 window.addEventListener("load", function() {
-    //Hide the success message
-    hideOutput();
-    //Store the entire form as a global variable
+    //Store the entire form
     var userForm = document.forms.userForm;
-    //Store the user input field as a global variable
-    var user = userForm.username;
-    //Store the password input field as a global variable
-    var password = userForm.password;
-
-
-    //Listen for this button to be clicked, then call the validateContent() function
+    //Listen for the form to be submitted
     userForm.addEventListener("submit", function(event) {
+        //Prevent the form from submitting.
         event.preventDefault();
-        if (!user.value || user.value === "") {
+        //Store the user input field
+        var username = userForm.username.value;
+        //Store the password input field
+        var password = userForm.password.value;
+        //Capture the hidden message field
+        var outMsg = document.getElementById("outMsg");
+        //Capture the return statement for the checkLogin() function
+        var result = checkLogin(username, password);
+
+        //Control structure for the results of 'checkLogin()'
+        if(result === true) {
+            outMsg.innerText = "Welcome Back!";
+        } else if (result === "No username entered.") {
+            outMsg.innerText = result;
             highlightUser();
-        } else if (!password.value || password.value === "") {
+        } else if (checkLogin(username, password) === "No password entered.") {
+            outMsg.innerText = result;
             highlightPassword();
-        } else {
-            //Reveal the hidden area
-            showContent();
-            //Display the validated credentials
-            displayCredentials();
+        } else if (checkLogin(username, password) === "Invalid Username or Password.") {
+            outMsg.innerText = "Invalid Username or Password";
+            highlightUser();
+            highlightPassword();
         }
     });
 
-    /*
-    * FUNCTIONS
-    */
+    function checkLogin(user, pass) {
+        //Hash the passed in password
+        var hashedUserPass = md5Encrypt(pass);
+        //Hash the actual password
+        var validPass = md5Encrypt("FoiledByDoyle3030");
+        //The valid user to compare against
+        var validUser = "SeanDoyle";
 
-
-
-    //Hides the output area below the form
-    function hideOutput() {
-        document.getElementById('hiddenArea').style.display = 'none';
+        //Validate if the user field is empty
+        if (user === "" || user === undefined || user === null || user === " "){
+            return "No username entered.";
+        } else if (pass === "" || pass === undefined || pass === null || pass === " ") {
+            return "No password entered.";
+        } else if (hashedUserPass !== validPass && user !== validUser) {
+            return "Invalid Username or Password.";
+        } else {
+            return true;
+        }
     }
-    //Show the output area below the form
-    function showContent() {
-        document.getElementById('hiddenArea').style.display = 'inline';
-    }
+
+    /**
+   * MD5 Encryption function to encrypt provided string.
+   * Returns string of encrypted hash.
+   * @param {string} stringIn
+   */
+    function md5Encrypt(stringIn) {
+        "use strict";
+        var md5string = new CryptoJS.MD5(stringIn);
+        return md5string.toString();
+    }//END md5Encrypt
+
     //Set the 'user' input field to pink
     function highlightUser() {
-        user.focus();
-        user.style.backgroundColor = "pink";
+        username.focus();
+        username.style.backgroundColor = "pink";
     }
     //Set the 'password' input field to pink
     function highlightPassword() {
         password.focus();
         password.style.backgroundColor = "pink";
-    }
-
-    function displayCredentials() {
-
-        //Capture the valid username
-        var validUser = userForm.username.value;
-        //Capture the valid password
-        var validPassword = userForm.password.value;
-        //Capture the paragraph for the username output
-        var userOutput = document.getElementById('validUser');
-        //Capture the paragraph for the password output
-        var passwordOutput = document.getElementById('validPassword');
-
-        //Populate the valid credentials
-        userOutput.innerText += ` ${validUser}`;
-        passwordOutput.innerText += ` ${validPassword}`;
     }
 });
